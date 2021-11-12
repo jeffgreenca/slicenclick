df9d6361258cc4601b4fd412a0a37ac51cc8944b
Jeff Green <jeffgreen@jpgit.com>
2021-11-11 18:39:38 -0800

    flailing

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ import { randomInt } from 'crypto';
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
+const worldSize = { w: 1024, h: 768 };
+
+class MyGame extends Phaser.Scene {
+    constructor() {
         super();
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image('logo', logoImg);
     }
-      
-    create ()
-    {
+
+    create() {
         const logo = this.add.image(400, 150, 'logo');
-      
+
         this.tweens.add({
             targets: logo,
             y: 450,
@@ -32,8 +30,10 @@ class MyGame extends Phaser.Scene
 class ClickLine extends Phaser.Scene {
     constructor() {
         super();
-        this.lastPos = {x: -1, y: -1};
+        this.lastPos = { x: -1, y: -1 };
         this.lineColor = 0xf807cc;
+        this.lineWidth = 2;
+        this.enemies = [];
     }
 
     preload() {
@@ -41,28 +41,33 @@ class ClickLine extends Phaser.Scene {
     }
 
     create() {
+        this.matter.world.setBounds(0, 0, worldSize.w, worldSize.h);
         this.input.on(Phaser.Input.Events.POINTER_DOWN, this.click, this)
 
+        // HERE see https://www.codeandweb.com/physicseditor/tutorials/how-to-create-physics-shapes-for-phaser-3-and-matterjs
+        // and https://github.com/photonstorm/phaser3-examples/blob/master/public/src/physics/arcade/sprite%20overlap%20group.js
+
         for (let index = 0; index < 15; index++) {
-            let x = this.randomIntFromInterval(0,1024);
-            let y = this.randomIntFromInterval(0,768);
-            let r = this.randomIntFromInterval(8,65);
+            let x = this.randomIntFromInterval(0, worldSize.w);
+            let y = this.randomIntFromInterval(0, worldSize.h);
+            let r = this.randomIntFromInterval(8, 65);
             let c = this.add.circle(x, y, r, 0x0808ee);
-            // let c = this.add.circle(x, y, r, this.randomIntFromInterval(10000,65500));
+            this.matter.add.circle(x,y,r, {isStatic: true, isSensor: true});
+            this.enemies.push(c)
             this.tweens.add({
                 targets: [c],
                 scale: 1.05,
                 yoyo: true,
                 repeat: -1,
-                duration: this.randomIntFromInterval(2000,3000),
-                ease: 'Sine.easeInOut',
-                delay: this.randomIntFromInterval(0,3000)
+                duration: this.randomIntFromInterval(2000, 3000),
+                ease: Phaser.Math.Easing.Sine.InOut,
+                delay: this.randomIntFromInterval(0, 3000)
             });
         }
 
     }
     randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
     update() {
@@ -78,36 +83,59 @@ class ClickLine extends Phaser.Scene {
         if (pointer.button !== 0) {
             return
         }
-        let c = this.add.circle(pointer.worldX, pointer.worldY, 10, this.lineColor);
+        let c = this.add.circle(pointer.worldX, pointer.worldY, 8, this.lineColor);
         this.tweens.add({
             targets: [c],
-            scale: 0.5,
+            scale: 0.4,
             yoyo: false,
-            duration: 400,
-            ease: 'Sine.easeInOut'
+            duration: 800,
+            ease: Phaser.Math.Easing.Expo.Out,
         });
         if (this.lastPos.x != -1) {
             // Nth click
-            this.add.line(
+            let line = this.add.line(
                 0,
                 0,
                 this.lastPos.x,
                 this.lastPos.y,
-                pointer.worldX, 
+                pointer.worldX,
                 pointer.worldY,
                 this.lineColor,
-            ).setOrigin(0)
+            ).setOrigin(0).setLineWidth(this.lineWidth);
+            this.matter.add.gameObject(line, {isSensor: true, isStatic: true});
+            // this.matter.add.gameObject(line);
+            // this.physics.world.overlap(line, this.enemies, this.collide)
         }
         this.lastPos.x = pointer.worldX;
         this.lastPos.y = pointer.worldY;
+    }
+
+    collide(e) {
+        console.log("COLLISION", e);
     }
 }
 
 const config = {
     type: Phaser.AUTO,
+    backgroundColor: 0x202020,
     parent: 'game',
-    width: 1024,
-    height: 768,
+    width: worldSize.w,
+    height: worldSize.h,
+    scale: {
+        // mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: {
+                y: 0,
+                x: 0,
+            },
+            debug: true,
+        }
+
+    },
     scene: ClickLine
 };
 
